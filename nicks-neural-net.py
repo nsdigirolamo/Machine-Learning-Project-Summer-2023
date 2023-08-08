70e71c4ae80411dfbbbe4ab4b10c73c1c438a724
nsdigirolamo <nsdigirolamo@gmail.com>
2023-08-07 20:13:23 -0400

    New stuff

--- nicks-neural-net.py
+++ nicks-neural-net.py
@@ -1,10 +1,12 @@
 from keras.models import Sequential
 from keras.layers import Dense
-from keras import optimizers
+from keras import optimizers, initializers, regularizers
 from keras import losses
 from keras import metrics
 from keras import Model
 from keras.callbacks import History
+
+import dill
 
 import matplotlib.pyplot as plt
 
@@ -19,34 +21,41 @@ from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
 
+import tensorflow_docs as tfdocs
+import tensorflow_docs.plots
+import tensorflow_docs.modeling
+
 DATASET_PATH = "new_vehicles.csv"
 IMAGE_DIR = "images/"
 
-LEARNING_RATE = 0.001
-STEPS_PER_EXECUTION = 100
-BATCH_SIZE = 32
-EPOCHS = 50
+LEARNING_RATE = 0.0001
+STEPS_PER_EXECUTION = 1
+BATCH_SIZE = 256
+EPOCHS = 10
+VALIDATION_SPLIT = 0.2
 
 
 def create_model() -> Model:
     
-    model = Sequential(
-        [
-            Dense(256, activation="relu"),
-            Dense(128, activation="relu"),
-            Dense(64, activation="relu"),
-            Dense(32, activation="relu"),
-            Dense(16, activation="relu"),
-            Dense(1, activation="sigmoid", name = "output"),
-        ]
-    )
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(256, input_dim=43),
+        tf.keras.layers.BatchNormalization(synchronized=True),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Dense(128),
+        tf.keras.layers.BatchNormalization(synchronized=True),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Dense(64),
+        tf.keras.layers.BatchNormalization(synchronized=True),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Dense(1)
+    ])
 
     model.compile(
-        optimizer = optimizers.Adam(
+        optimizer = optimizers.RMSprop(
             learning_rate = LEARNING_RATE
         ),
-        loss = losses.MeanAbsolutePercentageError(),
-        steps_per_execution = STEPS_PER_EXECUTION,
+        loss = losses.MeanSquaredError(),
+        steps_per_execution = None,
         metrics = [
             metrics.MeanAbsoluteError(),
         ],
@@ -56,12 +65,12 @@ def create_model() -> Model:
 
 
 def plot_loss(history: History):
-    plt.clf()
-    loss_values = history.history["loss"]
+    loss_values = history.history['loss']
     epochs = range(0, EPOCHS)
-    plt.plot(scalex = epochs, scaley = loss_values, label = "Training Loss per Epoch")
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
+    plt.clf()
+    plt.plot(epochs, loss_values, label='Training Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
     plt.legend()
     plt.savefig(f"{IMAGE_DIR}/loss_per_epoch.png")
 
@@ -75,32 +84,53 @@ def plot_results(y_actual: ndarray, y_pred: ndarray):
     plt.savefig(f"{IMAGE_DIR}/actual_vs_predicted.png")
 
 
-
 if __name__ == "__main__":
     
-    df: DataFrame = pd.read_csv(DATASET_PATH)
+    df: DataFrame = pd.read_csv(DATASET_PATH, encoding = "utf8")
 
+    labels = df.columns
     y = df["price"].values
     X = df.drop("price", axis = "columns").values
-    labels = df.drop("price", axis = "columns").columns
 
-    scaler = MinMaxScaler()
-    X_scaled = scaler.fit_transform(X)
+    X_train, X_test, y_train, y_test= train_test_split(X, y, test_size = 0.3, random_state = 42)
+    print(X_train.shape)
+    print(X_train[0])
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    print(X_train_scaled[0])
+    X_test_scaled = scaler.transform(X_test)
 
-    X_train, X_test, y_train, y_test= train_test_split(X_scaled, y, test_size = 0.3)
+    pd.DataFrame(X_train_scaled).describe().to_string("scaled.txt")
 
     model = create_model()
 
-    history = model.fit(
-        X_train[0:100],
-        tf.cast(y_train[0:100], tf.float32),
-        batch_size = BATCH_SIZE, 
-        epochs = EPOCHS,
-    )
+    # history = model.fit(
+    #     X_train_scaled,
+    #     tf.cast(y_train, tf.float32),
+    #     batch_size = BATCH_SIZE, 
+    #     epochs = EPOCHS,
+    #     validation_split = VALIDATION_SPLIT,
+    # )
 
-    y_pred = model(X_test)
+    # with open("dilledmodel", "wb") as dill_file:
+    #     dill.dump(model, dill_file)
 
-    plot_loss(history)
-    plot_results(y_test, y_pred)
+    # y_pred = model.predict(X_test_scaled).flatten()
 
-    model.summary()
+    # a = plt.axes(aspect='equal')
+    # plt.scatter(y_test, y_pred)
+    # plt.xlabel('True Values [Price]')
+    # plt.ylabel('Predictions [Price]')
+    # lims = [0, 100000]
+    # plt.xlim(lims)
+    # plt.ylim(lims)
+    # plt.plot(lims, lims)
+    # plt.savefig(f"{IMAGE_DIR}/scatter.png")
+
+    # plot_loss(history)
+
+    # y_pred = model.predict(X_test_scaled[0:10])
+
+    # plot_results(y_test[0:10], y_pred)
+
+    # model.summary()
